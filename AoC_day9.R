c80af64cea7b0991a6fc35821d31de99d8642bec
jmoggridge <ja.moggridge@gmail.com>
2021-01-09 23:08:31 -0500

    day10

--- AoC_day9.R
+++ AoC_day9.R
@@ -3,9 +3,9 @@
 # 
 # Though the port is non-standard, you manage to connect it to your computer through the clever use of several paperclips. Upon connection, the port outputs a series of numbers (your puzzle input).
 # 
-# The data appears to be encrypted with the eXchange-Masking Addition System (XMAS) which, conveniently for you, is an old cypher with an important weakness.
+# The data appears to be encrypted with the eXchange-Masking Addition System (xmas) which, conveniently for you, is an old cypher with an important weakness.
 # 
-# XMAS starts by transmitting a preamble of 25 numbers.
+# xmas starts by transmitting a preamble of 25 numbers.
 # After that, each number you receive should be the sum of any two of the 25 immediately previous numbers. The two numbers will have different values, and there might be more than one such pair.
 # 
 # For example, suppose your preamble consists of the numbers 1 through 25 in a random order. To be valid, the next number must be the sum of two of those numbers:
@@ -21,11 +21,12 @@
 # 64 and 66 would both be valid, as they are the result of 19+45 and 21+45 respectively.
 # Here is a larger example which only considers the previous 5 numbers (and has a preamble of length 5):
 #   
-#   35
+# 35
 # 20
 # 15
 # 25
 # 47
+
 # 40
 # 62
 # 55
@@ -43,20 +44,60 @@
 # 576
 # In this example, after the 5-number preamble, almost every number is the sum of two of the previous 5 numbers; the only number that does not follow this rule is 127.
 # 
-# The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
+# The first step of attacking the weakness in the xmas data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
+#   
+sums_in_range <- function(x, preamble){
+  return(preamble[-x] + preamble[x])
+}
+
+xmas <- as.numeric(readLines("input9.txt"))
+pre_length <- 25
+x <- 26
+
+while (T) {
+  preamble <- xmas[(x-pre_length):(x-1)]
+  v <- c(sapply(1:length(preamble), sums_in_range, preamble))
+  if (!xmas[x] %in% v){
+    cat('breaking at :', x)
+    break
+  }
+  x <- x+1
+}
+
+key <- xmas[x]
+key
+# 
+# --- Part Two ---
+#   The final step in breaking the xmas encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+# 
+# Again consider the above example:
+#   
+# xmas <- c(35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576)
+# In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+# 
+# To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+# 
+# What is the encryption weakness in your xmas-encrypted list of numbers?
 #   
 #   
-msg <- as.numeric(readLines("input9.txt"))
-t <- data.frame(i = 1:length(msg), 
-                x = rep(msg, length(msg)))
-t
-            
-i <- 6
-msg[i]
 
-sums <- msg[1:5]
+# holder for indices
+solution <- c(0)
+# create a sliding window that increments forward when the range's sum 
+# exceeds the 'key' value and increases in length while the range's sum is smaller that the key value.
+for (i in 1:(length(xmas)-1)) {
+  for (j in (i+1):length(xmas)) {
+    if (sum(xmas[i:j]) == key) {
+      solution <- c(i,j)
+      break
+    } else if (sum(xmas[i:j] > key)) break
+  }
+  if (length(solution) == 2) break
+}
 
-x <- 1
-msgy <- msg[-x]
-x <- x+msgy
-msg[i] %in% x
+# from the range that sums to the key, take the sum of the min and max values
+range <- xmas[solution[1]: solution[2]]
+weakness <- min(range) + max(range)
+cat('key: ', key)
+cat('weakness: ', weakness)
+
